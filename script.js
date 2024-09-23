80dbcfd37d215fcff0b3f9d64d9918b5868c0ff4
Ikadev95 <fegughi@gmail.com>
2024-09-23 15:58:59 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+class User {
+    constructor (_firstName, _lastName, _age, _location){
+        this.firstName = _firstName
+        this.lastName = _lastName
+        this.age = _age
+        this.location = _location
+        this.older = function (user2){
+            if (this.age > user2.age){
+                return this.firstName + " è più vecchio di " + user2.firstName
+            }
+            else return user2.firstName + " è più vecchio di " + this.firstName
+        }
+    }
+}
+
+const user1 = new User('Mario', 'Rossi', 35, 'Pavia');
+console.log(user1);
+
+const user2 = new User ('Paolo', 'Bianchi', 69, 'Torino');
+console.log(user2.older(user1));
+
+class Pet {
+    constructor (_petName, _ownerName, _species, _breed){
+        this.petName = _petName
+        this.ownerName = _ownerName
+        this.species = _species
+        this.breed = _breed
+        this.samePetOwner = function (pet2){
+            if (this.ownerName === pet2.ownerName){
+                return this.petName + " ha lo stesso padrone di " + user2.petName
+            }
+            return this.petName + " e " + user2.petName + " hanno padroni diversi "
+        }
+    }
+}
+
+const pets = [];
+const form = document.getElementById('form1')
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    let pName = document.getElementById('petName');
+    let oName = document.getElementById('ownerName');
+    let species = document.getElementById('species');
+    let breed = document.getElementById('breed');
+
+    const pet = new Pet (pName.value, oName.value ,species.value ,breed.value);
+
+    form.reset();
+    pets.push(pet);
+    console.log(pets);
+});
+
